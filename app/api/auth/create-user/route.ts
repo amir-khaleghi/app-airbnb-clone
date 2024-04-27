dbf9f17c50491a679dacd3bccf86fe5ebcce0ea2
amir-khaleghi <a.khaleghi.d@gmail.com>
2024-04-27 14:01:49 +0330

    [Error]: add error handling to Server api/auth/create-user

--- app/api/auth/create-user/route.ts
+++ app/api/auth/create-user/route.ts
@@ -1,33 +1,52 @@
 import db from '@/lib/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { error } from 'console';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+  try {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
 
-  if (!user || user === null || !user.id) {
-    console.error(error);
-    return NextResponse.json({ error: 'Invalid user' }, { status: 400 });
+    if (!user || user === null || !user.id) {
+      console.error('Invalid user');
+      return NextResponse.json({ error: 'Invalid user' }, { status: 400 });
+    }
+
+    let dbUser = await db.user
+      .findUnique({
+        where: { id: user.id },
+      })
+      .catch((err) => {
+        console.error('Error fetching user from database:', err);
+        throw err;
+      });
+
+    if (!dbUser) {
+      try {
+        dbUser = await db.user.create({
+          data: {
+            email: user.email ?? '',
+            firstName: user.given_name ?? '',
+            lastName: user.family_name ?? '',
+            id: user.id,
+            profileImage:
+              user?.picture ?? `https://avatar.vercel.sh/${user?.given_name}`,
+          },
+        });
+      } catch (err) {
+        console.error('Error creating user in database:', err);
+        throw err;
+      }
+    }
+
+    return NextResponse.redirect(
+      'https://app-airbnb-clone-amirdev.vercel.app/'
+    );
+  } catch (err) {
+    console.error('Internal Server Error:', err);
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
   }
-
-  let dbUser = await db.user.findUnique({
-    where: { id: user?.id },
-  });
-
-  if (!dbUser) {
-    dbUser = await db.user.create({
-      data: {
-        email: user.email ?? '',
-        firstName: user.given_name ?? '',
-        lastName: user.family_name ?? '',
-        id: user.id,
-        profileImage:
-          user?.picture ?? `https://avatar.vercel.sh/${user?.given_name}`,
-      },
-    });
-  }
-
-  return NextResponse.redirect('https://app-airbnb-clone-amirdev.vercel.app/');
 }
