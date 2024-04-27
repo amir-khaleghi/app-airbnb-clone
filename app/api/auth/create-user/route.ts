d2765d0580f00292102dee00d0b4ce6d14a9a348
amir-khaleghi <a.khaleghi.d@gmail.com>
2024-04-27 14:11:29 +0330

    [Error]: add more logs to server

--- app/api/auth/create-user/route.ts
+++ app/api/auth/create-user/route.ts
@@ -5,11 +5,14 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   try {
     const { getUser } = getKindeServerSession();
-    const user = await getUser();
+    const user = await getUser().catch((err) => {
+      console.error('Error fetching user from Kinde Auth:', err);
+      throw err;
+    });
 
     if (!user || user === null || !user.id) {
-      console.error('Invalid user');
-      return NextResponse.json({ error: 'Invalid user' }, { status: 400 });
+      console.error('Invalid user data:', user);
+      return NextResponse.json({ error: 'Invalid user data' }, { status: 400 });
     }
 
     let dbUser = await db.user
@@ -23,15 +26,28 @@ export async function GET() {
 
     if (!dbUser) {
       try {
+        const userData = {
+          email: user.email ?? '',
+          firstName: user.given_name ?? '',
+          lastName: user.family_name ?? '',
+          id: user.id,
+          profileImage:
+            user?.picture ?? `https://avatar.vercel.sh/${user?.given_name}`,
+        };
+
+        // Validate user data before creating
+        Object.keys(userData).forEach((key) => {
+          if (typeof userData[key] === 'undefined' || userData[key] === null) {
+            console.error(`Invalid value for ${key}: ${userData[key]}`);
+            return NextResponse.json(
+              { error: 'Invalid user data' },
+              { status: 400 }
+            );
+          }
+        });
+
         dbUser = await db.user.create({
-          data: {
-            email: user.email ?? '',
-            firstName: user.given_name ?? '',
-            lastName: user.family_name ?? '',
-            id: user.id,
-            profileImage:
-              user?.picture ?? `https://avatar.vercel.sh/${user?.given_name}`,
-          },
+          data: userData,
         });
       } catch (err) {
         console.error('Error creating user in database:', err);
